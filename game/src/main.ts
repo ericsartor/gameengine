65af4589015db1edacfcedaa183778aedfd167db
eric sartor <ericdavidsartor@live.ca>
2023-07-03 10:08:43 -0400

    update controllable pawn util to always move at a static speed

--- game/src/main.ts
+++ game/src/main.ts
@@ -3,6 +3,6 @@ import { followerTest } from './tests/follow';
 import { randomTest } from './tests/random';
 import { stageTest } from './tests/stage';
 
-// randomTest();
-followerTest();
+randomTest();
+// followerTest();
 // stageTest();
--- game/src/utils/controllableCharacter.ts
+++ game/src/utils/controllableCharacter.ts
@@ -14,15 +14,21 @@ export const addControllablePawn = (game: Game, pawnName?: string) => {
 		let pressedInputTimestamp = Infinity;
 		let animationToPlay = 'Idle';
 
+		let xMovement = 0;
+		let yMovement = 0;
+		const distance = deltaSeconds * 4;
+
 		let input = game.input.get('RunNorth');
 		if (input.pressed) {
-			game.getPawn(definedPawnName).moveRelative(0, deltaSeconds * -4);
+			// game.getPawn(definedPawnName).moveRelative(0, deltaSeconds * -4);
+			yMovement -= input.value * distance;
 			animationToPlay = 'RunNorth';
 			pressedInputTimestamp = input.timestampMs;
 		}
 		input = game.input.get('RunSouth');
 		if (input.pressed) {
-			game.getPawn(definedPawnName).moveRelative(0, deltaSeconds * 4);
+			// game.getPawn(definedPawnName).moveRelative(0, deltaSeconds * 4);
+			yMovement += input.value * distance;
 			if (input.timestampMs < pressedInputTimestamp) {
 				animationToPlay = 'RunSouth';
 				pressedInputTimestamp = input.timestampMs;
@@ -30,7 +36,8 @@ export const addControllablePawn = (game: Game, pawnName?: string) => {
 		}
 		input = game.input.get('RunWest');
 		if (input.pressed) {
-			game.getPawn(definedPawnName).moveRelative(deltaSeconds * -4, 0);
+			// game.getPawn(definedPawnName).moveRelative(deltaSeconds * -4, 0);
+			xMovement -= input.value * distance;
 			if (input.timestampMs < pressedInputTimestamp) {
 				animationToPlay = 'RunWest';
 				pressedInputTimestamp = input.timestampMs;
@@ -38,7 +45,8 @@ export const addControllablePawn = (game: Game, pawnName?: string) => {
 		}
 		input = game.input.get('RunEast');
 		if (input.pressed) {
-			game.getPawn(definedPawnName).moveRelative(deltaSeconds * 4, 0);
+			// game.getPawn(definedPawnName).moveRelative(deltaSeconds * 4, 0);
+			xMovement += input.value * distance;
 			if (input.timestampMs < pressedInputTimestamp) {
 				animationToPlay = 'RunEast';
 				pressedInputTimestamp = input.timestampMs;
@@ -46,5 +54,17 @@ export const addControllablePawn = (game: Game, pawnName?: string) => {
 		}
 		if (animationToPlay !== 'Idle') p.setAnimation(animationToPlay, timestampMs);
 		else p.stopAnimation();
+		if (xMovement !== 0 && yMovement !== 0) {
+			const hypotenuse = Math.sqrt(xMovement ** 2 + yMovement ** 2);
+			const xPercent = Math.abs(xMovement) / Math.abs(distance);
+			const yPercent = Math.abs(yMovement) / Math.abs(distance);
+			const averagePercent = (xPercent + yPercent) / 2;
+			const reductionRatio = (distance * averagePercent) / hypotenuse;
+			game
+				.getPawn(definedPawnName)
+				.moveRelative(xMovement * reductionRatio, yMovement * reductionRatio);
+		} else {
+			game.getPawn(definedPawnName).moveRelative(xMovement, yMovement);
+		}
 	});
 };
